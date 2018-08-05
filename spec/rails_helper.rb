35ce0a13705ce26e223ec9d007fbd56fc989c0d5
Brian Kichler <brian.kichler@gmail.com>
2018-08-05 16:51:52 -0700

    Heroku h10 crash on rspec, re-ran rspec installer

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,8 +54,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Devise::Test::ControllerHelpers, type: :view
-  include ActionDispatch::TestProcess
-  config.include FactoryBot::Syntax::Methods
 end
